61fd0b167ccbbffe20e926af914dea1b348d60a9
Thomas Schalch <thomas@thoang.ch>
2012-05-17 05:31:54 -0400

    added wiki output for PCRs

--- html/writeWiki.php
+++ html/writeWiki.php
@@ -8,10 +8,28 @@ print "</head><body>";
 include("title_bar.php");
 include("navigation.php");
 
+
 function wikiPrintEntry($entry){
+    global $userid, $groups;
     $wikiString = "| ${entry['trackID']} || ";
     $wikiString .= "[{{labdburl}}/editEntry.php?id=${entry['trackID']}&mode=display ${entry['name']}] || ";
-    $wikiString .= "${entry['description']}";
+    if(isset($entry['PCRoligo1'])){
+	$tmstring="";
+	for ($i=1;$i<=2;$i++){
+	    $oligoID=$entry["PCRoligo$i"];
+	    $o = getRecord($oligoID, $userid, $groups);
+	    $tm = $o['tm'] ? $o['tm'] : round(Tm($o['targetmatch'],'bre',$o['Saltconc']*1E-3, $o['PCRconc']*1E-9), 1);
+	    $wikiString .= "{{labdburl|id=$oligoID|label=${o['name']}}} ($oligoID) || ";
+	    $tmstring .= "$tm";
+	    $tmstring .= ($i == 1) ? "/" : "";
+	}
+	$wikiString .= "$tmstring || ";
+	$t = getRecord($entry['PCRtemplate'], $userid, $groups);
+	$wikiString .= "{{labdburl|id=${entry['PCRtemplate']}|label=${t['name']}}} (${entry['PCRtemplate']}) || ";
+	$wikiString .= CountATCG($entry['DNASequence'])." bp ";
+    }else{
+	$wikiString .= "[{{labdburl|id=${entry['description']}}} (=${entry['description']})";
+    }
     return $wikiString;
 }
 
@@ -20,7 +38,11 @@ if ($_GET["output"] and $_GET["selection"]){
 	$selection = explode(",", $_GET["selection"]);
 	$wikiOutput = "{|class=\"wikitable\"<br/>\n";
 	$wikiOutput .= "|+Construct<br/\n>";
-	$wikiOutput .= "! ID !! Construct !! Description<br/>\n";
+	if(isset($selection[0]['PCRoligo1'])){
+	    $wikiOutput .= "! ID !! Construct !! Oligo 1 !! Oligo 2 !! Tm !! Template !! Product length<br/>\n";
+	} else {
+	    $wikiOutput .= "! ID !! Construct !! Description<br/>\n";
+	}
 	foreach ($selection as $entry){
 	    $entry = getRecord($entry, $userid, $groups);
 	    $wikiOutput .= "|-<br/>" . wikiPrintEntry($entry) . "<br/>\n";
