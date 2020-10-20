d565a0bc3b0b8c014be4d5f3b88e9412abd5df75
Thomas Schalch <thomas@thoang.ch>
2020-10-20 22:22:02 +0100

    removed bdd

--- html/bdd.php
+++
@@ -1,9 +0,0 @@
-<?php
-try
-{
-	$bdd = new PDO('mysql:host=localhost;dbname=lab_labdb;charset=utf8', 'calendar', 'gJ17y#eskC2^');
-}
-catch(Exception $e)
-{
-        die('Erreur : '.$e->getMessage());
-}
